709332a75ec29e4da5ce802ef41f22ba91c7cac9
Matt <higginsmatt321@gmail.com>
2019-09-17 15:56:50 +1000

    adding bash scripts

---
+++ script1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# create a new folder called "counting"
+mkdir counting
+# change into the new counting folder
+cd counting
+# Create a new file in the counting folder call it counting.txt
+touch counting.txt
+# print the text counting to ten
+echo "counting to ten" 
+# write code that prints 1-10 new number each line in counting.txt 
+echo "using for loop method # 1...."
+for (( i=1; i<=10; i++))
+do 
+ echo "$i"
+done 
---
+++ script2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# create a new folder called "counting"
+mkdir counting
+# change into the new counting folder
+cd counting
+# Create a new file in the counting folder call it counting.txt
+touch counting.txt
+# print the text counting to ten
+echo "counting to ten" 
+# write code that prints 1-10 new number each line in counting.txt 
+echo "1" > counting.txt
+echo "2" >> counting.txt
+echo "3" >> counting.txt
+echo "4" >> counting.txt
+echo "5" >> counting.txt
+echo "6" >> counting.txt
+echo "7" >> counting.txt
+echo "8" >> counting.txt
+echo "9" >> counting.txt
+echo "10" >> counting.txt
+
